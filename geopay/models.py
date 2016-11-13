6795762e167684c628b9b97bf80b56fa32d332ec
Kevin Lee <leekevin@berkeley.edu>
2016-11-12 20:03:20 -0800

    Add products

--- geopay/models.py
+++ geopay/models.py
@@ -5,7 +5,12 @@ class Customer(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
 
+class Products(models.Model):
+    name = models.CharField(max_length=30)
+    price = models.DecimalField(decimal_places=2, max_digits=20)
+
 class Merchant(models.Model):
     merchant_id = models.CharField(max_length=30)
     longitude = models.DecimalField(decimal_places=5, max_digits=10)
     latitude = models.DecimalField(decimal_places=5, max_digits=10)
+    products = models.ManyToManyField(Products)
